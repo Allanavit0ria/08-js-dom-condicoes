31268f7fd31d4a60f80ec20b874fb4463e621409
Allanavit0ria <132669005+Allanavit0ria@users.noreply.github.com>
2023-06-25 14:01:09 +0000

    funciona

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
-    var data = new data()
+    var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = 'Agora são ${hora} horas.'
     if  (hora >= 0 && hora < 12){
