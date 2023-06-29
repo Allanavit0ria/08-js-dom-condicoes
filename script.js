8429ed38987b4b0a5144c0ebd7d3292ac68d06ff
Allanavit0ria <132669005+Allanavit0ria@users.noreply.github.com>
2023-06-29 12:57:12 +0000

    Atualizado

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = 'Agora são' ${hora} 'horas.'
-    if  (hora >= 0 && hora < 12){
+msg.innerHTML = ´Agora são ${hora} horas.´
+     if  (hora >= 0 && hora < 12){
         // BOM DIA! 
         img.src = 'fotomanha.png'
         document.body.style.background = '#e2cd9f'
