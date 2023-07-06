3897d28657ff079cd67cdf05b23ec491d451ec66
Allana <avnr@aluno.ifnmg.edu.br>
2023-07-06 14:56:14 +0000

    script.js

--- modelo02/script.js
+++ modelo02/script.js
@@ -3,21 +3,20 @@ function verificar () {
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res') 
-
     if (fano.value.length == 0 || Number(fano.value) > ano){
         window.alert('[ERROR] Verifique os dados e tente novament!')
-    }  else {
-        var fsex = document.getElementByName('radsex') 
+    } else {
+        var fsex = document.getElementsByName('radsex') 
         var idade =  ano - Number(fano.value)
         var gênero = '' 
         var img =  document.createElement('img')
         img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             gênero = 'Homem' 
-            if (idade >=0 && idade < 10){
+            if (idade >=0 && idade < 10) {
                 //Criança
                 img.setAttribute('src', 'foto-bebe-homem.png')
-            } else if (idade < 21){
+            } else if (idade < 21) {
                 //Jovem
                 img.setAttribute('src', 'foto-jovem-homem.png')
             } else if (idade < 50 ) {
@@ -30,10 +29,10 @@ function verificar () {
             
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
-            if (idade >=0 && idade < 10){
+            if (idade >=0 && idade < 10) {
                 //Criança
                 img.setAttribute('src', 'foto-bebe-mulher.png')
-            } else if (idade < 21){
+            } else if (idade < 21) {
                 //Jovem
                 img.setAttribute('src', 'foto-jovem-mulher.png')
             } else if (idade < 50 ) {
@@ -44,8 +43,8 @@ function verificar () {
                 img.setAttribute('src', 'foto-idosa-mulher.png')
             }
         }
-        res.computedStyleMap.textAlign = 'center' 
-        res.innnerHTML = `Detectamos ${gênero} com ${idade} anos.` 
+        res.style.textAligt = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.` 
         res.appendChild(img) 
     }
 }
